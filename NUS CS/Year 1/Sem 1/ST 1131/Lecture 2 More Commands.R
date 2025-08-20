06de56122f04eb1cdcda2725c6e6d8ca221a7a52
Rubyherp <horxiangneng123@gmail.com>
2025-08-21 01:36:36 +0800

    Create Lecture 2 More Commands.R

---
+++ NUS CS/Year 1/Sem 1/ST 1131/Lecture 2 More Commands.R
@@ -0,0 +1,51 @@
+setwd("C:/DATA1131")
+data1 = read.csv("lung_cancer.csv", sep = ",", header = TRUE); data1
+
+   Cancer Age Gender Smoke
+1       1  68      1     1
+2       1  69      0     1
+3       1  55      1     1
+4       1  48      1     1
+5       1  73      0     1
+6       1  76      0     1
+7       1  66      1     0
+8       1  61      0     0
+9       0  40      1     1
+10      0  53      0     1
+11      1  49      0     1
+12      0  53      0     0
+13      0  39      0     0
+14      0  71      1     0
+15      0  36      1     0
+16      0  25      0     0
+17      0  40      0     0
+18      0  45      0     0
+19      0  30      0     0
+20      1  37      0     0
+21      1  67      1     0
+22      1  55      1     1
+23      1  49      1     0
+24      0  58      1     0
+25      0  70      1     0
+26      1  65      1     1
+27      1  48      1     0
+28      1  54      1     1
+29      1  49      1     1
+30      0  47      0     0
+31      0  68      0     1
+32      0  46      0     1
+33      0  31      1     0
+34      0  38      1     0
+
+female = sum(data1$Gender == 0)
+male = sum(data1$Gender == 1)
+gender = data.frame(female, male)
+
+  female male
+1     16   18
+
+gender = gender / (16+18) * 100; gender  #AS A PERCENTAGE / PROPORTION
+
+    female     male
+1 47.05882 52.94118
+
