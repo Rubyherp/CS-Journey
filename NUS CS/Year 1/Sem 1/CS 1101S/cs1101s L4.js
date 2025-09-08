8648eda8d226675b23ce7bfb5eff6b9732085d2d
Rubyherp <horxiangneng123@gmail.com>
2025-09-08 14:08:55 +0800

    Create cs1101s L4.js

---
+++ NUS CS/Year 1/Sem 1/CS 1101S/cs1101s L4.js
@@ -0,0 +1,41 @@
+const pair = (x, y) => f => f(x,y);
+const head = p => p((x, y) => x);
+const tail = p => p((x, y) => y);
+
+//head(pair(3, 5));
+//tail(pair(3, 5));
+
+
+
+function make_rational(x, y){
+    return pair(x,y);
+}
+
+function numerator(x){
+    return head(x);
+}
+
+function denominator(x){
+    return tail(x);
+}
+
+function add_rational(x,y){
+    return make_rational(numerator(x) * denominator(y)+ numerator(y) * denominator(x)
+        ,
+        denominator(x) * denominator(y));
+}
+
+//Greatest common divisor
+function gcd(a,b){
+    return b === 0 ? a : gcd(b, a%b);
+}
+// Code might be wrong
+function new_make_rational(x,y){
+    const g = gcd(x,y);
+    return pair(x/g, y/g);
+}
+//const one_half = make_rational(1,2);
+
+
+
+
